a95246d1335d2cf6de3d1d2042c19fc46058495e
always-smiling <light.apple.juice@gmail.com>
2016-09-10 23:23:37 +0300

    UserImpl

---
+++ user/UserImpl.py
@@ -0,0 +1,21 @@
+from settings import settings
+import logging
+
+__author__ = 'g.lavrentyeva'
+
+class User:
+    def __init__(self):
+        self.config = settings()
+
+        self.logger = logging.getLogger('BotLogger.User')
+        self.logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler(self.config.log)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+        self.superuser = False
+
+    def SetSuperUser(self):
+        self.logger.info('SetSuperUser')
+        self.superuser = True
