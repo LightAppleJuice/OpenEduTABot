999d790c6d7ea96c8f1967f41be4da5e9ec1231f
always-smiling <light.apple.juice@gmail.com>
2016-09-25 19:57:12 +0300

    WorkWithUsersData temporary came back

--- MySQL_api/Commands.py
+++ MySQL_api/Commands.py
@@ -161,6 +161,81 @@ class workWithData:
             verified = self.cursor.execute(sqlStr).fetchall()
         return {'answered': answered, 'checked': checked, 'verified': verified}
 
+    def getUsers(self):
+        """
+        Получение всех пользователей из таблицы
+        :return: Список id всех пользователей
+        """
+        sqlStr = "SELECT id_sender, id_responder, wrongResponders FROM questions"
+        base = {}
+        with self.connection:
+            temp = self.cursor.execute(sqlStr).fetchall()
+        for elem in temp:
+            print elem
+        return base
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
+        self.logger.info('Closed')
+
+class workWithUsersData:
+
+    def __init__(self):
+        self.config = settings()
+
+        self.logger = logging.getLogger('BotLogger.workWithUsersData')
+        self.logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler(self.config.log)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+        self.connection = sqlite3.connect(self.config.users_name, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('''CREATE TABLE if not exists users (id_token COUNTER CONSTRAINT PrimaryKey PRIMARY KEY ,
+                                                                     statistics INTEGER)''')
+
+        self.logger.info('Init done')
+
+    def addRow(self, id_token, statistics):
+        """
+        Добавление новой записи в таблицу
+        :param question: string вопрос
+        :param answer: string ответ
+        """
+        sqlStr = "INSERT INTO users VALUES ({0}, {1})".format(id_token, statistics)
+        sqlStrDel = "DELETE FROM users WHERE id_token={0}".format(id_token)
+
+        with self.connection:
+            self.cursor.execute(sqlStrDel)
+            self.cursor.execute(sqlStr)
+
+        self.logger.info('Added user, id_token: %s' % id_token)
+
+    def readRows(self):
+        """
+        Получаем все строки базы данных
+        :return: список вопросов и ответов
+        """
+        base = {}
+        with self.connection:
+            temp = self.cursor.execute('SELECT * FROM users').fetchall()
+        for elem in temp:
+            base[elem[0]] = elem[1]
+        self.logger.info('Read Rows')
+        return base
+
+    def removeRow(self, id_token):
+        """
+        Удаляем запись адреса
+        :param question: string вопрос
+        """
+        sqlStrDel = "DELETE FROM users WHERE id_token={0}".format(id_token)
+        with self.connection:
+            self.cursor.execute(sqlStrDel)
+        self.logger.info('Row removed id_token: %s' % id_token)
+
     def __del__(self):
         self.cursor.close()
         self.connection.close()
